85e4d23293f59c4cfd5dbeb7803ce48e3d8712e0
Subham-Joshi <sj81official@gmail.com>
2025-01-09 12:44:34 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,48 @@
 #include<iostream>
-                   
+#include <queue>
+#include <vector>
 using namespace std;
-
-int main(){
-
-  return 0 ;
-
-
-
-
-
-
+class Priority{
+    public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    void checkP(int priority){
+        if (priority ==0){
+            cout<<"standard Booking"<<endl;
+        }
+        else if (priority ==1){
+            cout<<"Vip Booking"<<endl;
+        }
+        else{
+            cout<<"Internal error"<<endl;
+        }
+    }
+    void Book(int Booking){                            
+        if(pq.size()==10){
+            cout<<" Bookings Full!"<<endl;
+            return;
+        }
+        else{
+            pq.push(Booking);
+            cout<<"Booking added"<<endl;
+        }
+    }    
+    void Cancellation(){                            
+        if(pq.empty()){
+            cout<<"NO Bookings yet!"<<endl;
+            return;
+        }
+        else{
+            pq.pop();
+            cout<<"Cancellation Succesfull"<<endl;
+        }
+    }    
+};
+int main() {
+    int priority=1;
+    int Booking=10;
+    Priority obj;
+    obj.checkP(priority);
+    obj.Cancellation();
+    obj.Book(Booking);
+    return 0;
 }
