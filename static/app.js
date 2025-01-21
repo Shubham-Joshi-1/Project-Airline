9b08569dc7420bdb0d06e18759d3231d25fc17fb
Subham-Joshi <sj81official@gmail.com>
2025-01-21 19:48:53 +0530

    Frontend-Node-MongoDB connection Established(Localhost running Successfully)

--- static/app.js
+++ static/app.js
@@ -6,6 +6,12 @@ const path = require("path");
 const app = express();
 const staticPath = path.join(__dirname, "../static");
 
+mongoose.connect("mongodb+srv://Ansh:airline123@cluster0.zycn0.mongodb.net/");
+const db = mongoose.connection;
+
+db.on("error", () => console.log("Error connecting to the database"));
+db.once("open", () => console.log("MongoDb connected"));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(staticPath));
