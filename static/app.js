cc3103a81d4813b365ecb7bbb1f1e47165985dcc
Subham-Joshi <sj81official@gmail.com>
2025-01-27 10:57:10 +0530

    updated

--- static/app.js
+++ static/app.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
-
 const app = express();
 const staticPath = path.join(__dirname, "../static");
 
@@ -12,20 +10,34 @@ const db = mongoose.connection;
 db.on("error", () => console.log("Error connecting to the database"));
 db.once("open", () => console.log("MongoDb connected"));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.use(express.static(staticPath));
-app.use('/static', express.static(path.join(__dirname, '../static')));
-app.use(express.json())
+console.log("Departure:");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/index.html", async (req, res) => {
-    const { departure, arrival , departureDate, returnDate,passengers } = req.body; 
+app.post("/available_flights", async (req, res) => {
+    console.log("Departure:1");
+    const { tripType,from, to, departureDate, passengers } = req.body; 
+    console.log("Departure2:"); 
     console.log(req.body);
+    console.log("Departure3:");
+    console.log("Departure:4"); 
+           
 });
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: path.join(__dirname, '../static') });
+app.use((req, res, next) => {
+  console.log(`${req.method} request to ${req.url}`);
+  next();
 });
+
+app.get("/", (req, res) => {
+    res.sendFile("index.html", { root: staticPath });
+  });
+  app.get("/available_flights", (req, res) => {
+    res.sendFile("/available_flights.html", { root: staticPath });
+  });
 
 app.listen(4001, () => {
         console.log("Listening on port 4001");
