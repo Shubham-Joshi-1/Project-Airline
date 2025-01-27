4ee3597da30141ebcdc88b818b9404ce9e6c1f88
Subham-Joshi <sj81official@gmail.com>
2025-01-27 12:37:37 +0530

    updated

--- static/app.js
+++ static/app.js
@@ -18,25 +18,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.post("/available_flights", async (req, res) => {
-    console.log("Departure:1");
     const { tripType,from, to, departureDate, passengers } = req.body; 
-    console.log("Departure2:"); 
-    console.log(req.body);
-    console.log("Departure3:");
-    console.log("Departure:4"); 
+    console.log(req.body); 
+    res.redirect("available_flights");
            
 });
-
-app.use((req, res, next) => {
-  console.log(`${req.method} request to ${req.url}`);
-  next();
+app.post("/user_info", async (req, res) => {
+    console.log("Departure:1");
+    const { email,first_name, last_name, month, day, year,gender} = req.body; 
+    console.log(req.body); 
+    res.redirect("/user_info");         
 });
+
+// app.use((req, res, next) => {
+//   console.log(`${req.method} request to ${req.url}`);
+//   next();
+// });
 
 app.get("/", (req, res) => {
     res.sendFile("index.html", { root: staticPath });
   });
   app.get("/available_flights", (req, res) => {
     res.sendFile("/available_flights.html", { root: staticPath });
+  });
+  app.get("/user_info", (req, res) => {
+    res.sendFile("/user_info.html", { root: staticPath });
   });
 
 app.listen(4001, () => {
